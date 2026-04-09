967af7f39b339fb83a5a93ff36909a7779f10ffc
Andreas Ebner <andreas.ebner@o6-automation.com>
2026-04-09 22:27:13 +0200

    feat(sec): add ECC_curve448 security policy

--- plugins/include/open62541/server_config_default.h
+++ plugins/include/open62541/server_config_default.h
@@ -263,6 +263,11 @@ UA_ServerConfig_addSecurityPolicyEccCurve25519(UA_ServerConfig *config,
                                                const UA_ByteString *certificate,
                                                const UA_ByteString *privateKey);
 
+UA_EXPORT UA_StatusCode
+UA_ServerConfig_addSecurityPolicyEccCurve448(UA_ServerConfig *config,
+                                             const UA_ByteString *certificate,
+                                             const UA_ByteString *privateKey);
+
 /* Adds all supported security policies and sets up certificate
  * validation procedures.
  *
@@ -318,6 +323,11 @@ UA_EXPORT UA_StatusCode
 UA_ServerConfig_addSecurityPolicyEccCurve25519(UA_ServerConfig *config,
                                                const UA_ByteString *certificate,
                                                const UA_ByteString *privateKey);
+
+UA_EXPORT UA_StatusCode
+UA_ServerConfig_addSecurityPolicyEccCurve448(UA_ServerConfig *config,
+                                             const UA_ByteString *certificate,
+                                             const UA_ByteString *privateKey);
 
 /* Adds all supported security policies and sets up certificate
  * validation procedures.
